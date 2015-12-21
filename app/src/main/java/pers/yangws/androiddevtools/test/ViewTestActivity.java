a86f4483f79edfe947a62bd568fad7aa71418790
qq272936993 <272936993@qq.com>
2015-12-21 18:11:55 +0800

    update directory

---
+++ app/src/main/java/pers/yangws/androiddevtools/test/ViewTestActivity.java
@@ -0,0 +1,19 @@
+package pers.yangws.androiddevtools.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import pers.yangws.androiddevtools.R;
+
+/**
+ * Created by 1yyg-安卓 on 2015/12/21.
+ */
+public class ViewTestActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
